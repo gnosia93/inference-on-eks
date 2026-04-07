737eea04b09de3f4c366d1a858732e1f6c4eff00
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-04-07 16:20:51 +0900

    Create karpenter.tf

---
+++ iac/tf/karpenter.tf
@@ -0,0 +1,55 @@
+# ============================================
+# Karpenter Helm Install
+# ============================================
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    }
+  }
+}
+
+resource "helm_release" "karpenter" {
+  name       = "karpenter"
+  repository = "oci://public.ecr.aws/karpenter"
+  chart      = "karpenter"
+  version    = "1.3.0"
+  namespace  = "karpenter"
+  create_namespace = true
+
+  set {
+    name  = "settings.clusterName"
+    value = aws_eks_cluster.main.name
+  }
+
+  set {
+    name  = "settings.clusterEndpoint"
+    value = aws_eks_cluster.main.endpoint
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.karpenter_controller.arn
+  }
+
+  set {
+    name  = "controller.resources.requests.cpu"
+    value = "1"
+  }
+
+  set {
+    name  = "controller.resources.requests.memory"
+    value = "1Gi"
+  }
+
+  depends_on = [
+    aws_eks_node_group.system,
+    aws_iam_role_policy_attachment.karpenter_policy,
+    aws_iam_role_policy_attachment.karpenter_controller,
+  ]
+}
