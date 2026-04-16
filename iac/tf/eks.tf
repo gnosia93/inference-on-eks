9451325c2f69053df5b08be73a16d117950c25dc
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-04-16 17:17:10 +0900

    Update eks.tf

--- iac/tf/eks.tf
+++ iac/tf/eks.tf
@@ -57,6 +57,18 @@ resource "aws_eks_access_policy_association" "ec2_bastion_admin" {
   depends_on = [aws_eks_access_entry.ec2_bastion]
 }
 
+# EC2 → EKS API 서버 443 허용
+resource "aws_security_group_rule" "ec2_to_eks" {
+  type                     = "ingress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
+  source_security_group_id = aws_security_group.instance_sg.id
+}
+
+
+
 ### 추가 끝 ###
 
 
