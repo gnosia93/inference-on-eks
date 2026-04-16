09e8f60a716240fa8449d4a284d41c9bac5c242b
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-04-16 22:29:01 +0900

    Create s3.tf

---
+++ iac/tf/s3.tf
@@ -0,0 +1,24 @@
+# IAM Role for S3 access (IRSA)
+resource "aws_iam_role" "s3_access" {
+  name = "${var.cluster_name}-s3-access"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      },
+      Action = "sts:AssumeRoleWithWebIdentity",
+      Condition = {
+        StringEquals = {
+          "${replace(aws_eks_cluster.main.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:default:s3-access-sa"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  role       = aws_iam_role.s3_access.name
+}
