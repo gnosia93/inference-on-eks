19069ab6d73945bcecfd6c5bc87afa915d2f6d28
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-04-18 19:56:04 +0900

    Update outputs.tf

--- iac/tf/outputs.tf
+++ iac/tf/outputs.tf
@@ -6,6 +6,16 @@ output "eks_cluster_name" {
   value = aws_eks_cluster.main.name
 }
 
+output "milvus_role_arn" {
+  value = aws_iam_role.milvus.arn
+}
+
+output "vectordb_bucket_name" {
+  value       = aws_s3_bucket.milvus.id
+  description = "Milvus S3 bucket name"
+}
+
+
 #output "eks_cluster_endpoint" {
 #  value = aws_eks_cluster.main.endpoint
 #}
